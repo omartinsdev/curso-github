abc6fbaaedd0a5b9afce1288ce7933eef0075286
omartinsdev <alison.pessoal.dev@gmail.com>
2025-05-06 23:45:09 -0300

    Criando um sistema ficticio de login

--- login.js
+++ login.js
@@ -1,3 +1,23 @@
 // Trabalhando no login....
 
 // Finalizando sistema de login.
+class LoginSystem {
+  constructor(name, email, address, phone) {
+    this.name = name;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+  }
+
+  signInClient(clientNameAuth) {
+    if (clientNameAuth === this.name) {
+      console.log(`Welcome ${this.name}! You are signed!`);
+    } else {
+      console.log(`You dont have signed up. Please sign up.`);
+    }
+  }
+
+  signUpNewClient(newClientName) {
+    console.log("You are signed up!!");
+  }
+}
